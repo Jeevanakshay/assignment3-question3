e3ed7c95e3122bf4a425b8ae6bc697754e719154
Jeevanakshay <84185011+Jeevanakshay@users.noreply.github.com>
2022-01-23 18:09:58 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,17 @@
+import X from "./X";
+import Y from "./Y";
+import Z from "./Z";
+
+
+function App() {
+  return (
+    <>
+    <X a="13" b="2"  />
+    <Y a="1" b="2" c="2"/>
+    <Z/>
+    </>
+  
+  );
+}
+
+export default App;
